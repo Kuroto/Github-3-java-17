8c1ab5d9e6a36cb44d0cde4dd5388263dadc421d
Kuroto Robert <kuroto@randompolygons.com>
2025-01-09 20:29:48 +0100

    My first commit

---
+++ src/test/java/calculator/MyClassTest.java
@@ -0,0 +1,21 @@
+package calculator;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyClassTest
+{
+    @Test
+    public void testAdd()
+    {
+        MyClass myClass = new MyClass();
+
+        // Adding a comment to commit
+        // Opps, did something wrong, redoing the comment
+        // Testing more
+        assertEquals(3, myClass.add(1, 2));
+        assertEquals(0, myClass.add(-1, 1));
+        assertEquals(0, myClass.add(0, 0));
+    }
+}
